f0281751630c91fd90c9e8f607b9b6c1e5c057b5
Gabriel Cretin <gabrielcretin@gmail.com>
2023-05-07 22:43:59 +0200

    KPAX executable is already installed

--- install.sh
+++ install.sh
@@ -10,19 +10,4 @@ else
     exit 1
 fi
 
-# Install KPAX
-pushd bin >/dev/null 2>&1
-wget http://kpax.loria.fr/download/kpax-5.1.3-x64-mint18.3.tgz >/dev/null 2>&1
-tar -xf kpax-5.1.3-x64-mint18.3.tgz
-rm -f kpax-5.1.3-x64-mint18.3.tgz
-
-if [ -f kpax/bin/kpax5.1.3.x64 ]
-then
-    ln -s kpax5.1.3.x64 kpax/bin/kpax5.0.2.x64
-    echo "Installed KPAX"
-else
-    echo "An error occured, KPAX could not be installed in bin/"
-    exit 1
-fi
-popd >/dev/null 2>&1
 echo "All set, good to go !"
